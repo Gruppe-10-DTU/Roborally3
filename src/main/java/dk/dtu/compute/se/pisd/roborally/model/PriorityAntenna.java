246d9aa01c455d2da0f329425f584bbe1524b125
sand4570 <sand4570@stud.kea.dk>
2023-04-19 12:13:23 +0200

    create PriorityAntenna class

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/PriorityAntenna.java
@@ -0,0 +1,13 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+public class PriorityAntenna {
+    private Space space;
+
+    public PriorityAntenna (Space space) {
+        this.space = space;
+    }
+
+    public Space getSpace() {
+        return space;
+    }
+}
