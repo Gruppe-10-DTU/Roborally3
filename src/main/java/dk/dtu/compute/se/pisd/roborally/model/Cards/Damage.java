4437dd5f9f9a69b4f46f87fd305f16940f1e4c9f
Soren <sorenfw@outlook.com>
2023-04-20 16:15:50 +0200

    Written Damage.java

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Cards/Damage.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Cards/Damage.java
@@ -1,4 +1,27 @@
 package dk.dtu.compute.se.pisd.roborally.model.Cards;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public enum Damage {
+
+    SPAM("SPAM", 20),
+    TROJANHORSE("Trojan Horse",18),
+    WORM("WORM",18),
+    VIRUS("VIRUS",18);
+
+    final public String displayName;
+
+    final int cardAmount;
+
+    Damage(String displayName,int cardAmount) {
+        this.displayName = displayName;
+        this.cardAmount = cardAmount;
+    }
+
+
+    public int getAmount(){
+        return this.cardAmount;
+    }
 }
