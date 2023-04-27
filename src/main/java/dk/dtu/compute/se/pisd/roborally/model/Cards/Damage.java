c4e353a4b761e1ec4f5626d6e2dbd96591a3f803
Philip Astrup Cramer <s224319@dtu.dk>
2023-04-27 15:27:23 +0200

    added optional type to handle when out of one type of cards

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Cards/Damage.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Cards/Damage.java
@@ -9,18 +9,33 @@ public enum Damage {
     SPAM("Spam", 20),
     TROJAN_HORSE("Trojan Horse",18),
     WORM("Worm",18),
-    VIRUS("Virus",18);
+    VIRUS("Virus",18),
+    OPTIONAL("Player Choice",0, SPAM, TROJAN_HORSE, VIRUS, WORM);
 
-    final public String displayName;
+    final private String displayName;
 
-    final int cardAmount;
+    final private int cardAmount;
+    private List<Damage> options;
 
-    Damage(String displayName,int cardAmount) {
+
+    public List<Damage> getOptions() {
+        return options;
+    }
+
+    Damage(String displayName, int cardAmount, Damage... options) {
         this.displayName = displayName;
         this.cardAmount = cardAmount;
+        this.options = Arrays.asList(options);
     }
 
     public int getAmount(){
         return this.cardAmount;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+    public void removeOption(Damage option){
+        this.options.remove(option);
     }
 }
