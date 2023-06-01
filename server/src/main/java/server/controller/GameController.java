e15532d64bcbfdf2acb43fc8ff9e3ba6cdabe3f4
Soren <sorenfw@outlook.com>
2023-06-01 15:13:31 +0200

    Created getGameList()

--- server/src/main/java/server/controller/GameController.java
+++ server/src/main/java/server/controller/GameController.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class GameController {
+    private ArrayList<String> gameList = new ArrayList<>();
     @RequestMapping(value = "/games", method = RequestMethod.GET)
-    public String getGame(){
-        return "Test is nice";
+    public ArrayList<String> getGameList(){
+        return gameList;
     }
 }
