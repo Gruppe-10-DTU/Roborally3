cdf34c8aa591587922b616845b4f60de3e3aef41
Philip Astrup Cramer <s224319@dtu.dk>
2023-06-01 10:24:22 +0200

    removed unused card amount

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Cards/Damage.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Cards/Damage.java
@@ -2,26 +2,20 @@ package dk.dtu.compute.se.pisd.roborally.model.Cards;
 
 public enum Damage {
 
-    SPAM("Spam", 20),
-    TROJAN_HORSE("Trojan Horse", 18),
-    WORM("Worm", 18),
-    VIRUS("Virus", 18);
+    SPAM("Spam"),
+    TROJAN_HORSE("Trojan Horse"),
+    WORM("Worm"),
+    VIRUS("Virus");
 
     final public String displayName;
 
-    final int cardAmount;
 
     /**
      * @param displayName The name of the card
-     * @param cardAmount  How many cards to create
      * @author Philip Astrup Cramer
      */
-    Damage(String displayName, int cardAmount) {
+    Damage(String displayName) {
         this.displayName = displayName;
-        this.cardAmount = cardAmount;
     }
 
-    public int getAmount() {
-        return this.cardAmount;
-    }
 }
