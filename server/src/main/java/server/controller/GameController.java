06246c4517761c3dba423fd0016d47ff1db40a84
Søren s194630 <arcspider45@gmail.com>
2023-06-02 09:03:19 +0200

    Added non-working endpoints

--- server/src/main/java/server/controller/GameController.java
+++ server/src/main/java/server/controller/GameController.java
@@ -1,8 +1,10 @@
 package server.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.ArrayList;
 
@@ -12,5 +14,17 @@ public class GameController {
     @RequestMapping(value = "/games", method = RequestMethod.GET)
     public ArrayList<String> getGameList(){
         return gameList;
+    }
+    @RequestMapping(value = "/games/{id}", method = RequestMethod.GET)
+    public ResponseEntity<String> getSpecificGame() throws HttpServerErrorException.NotImplemented {
+        return null;
+    }
+    @RequestMapping(value = "/games", method = RequestMethod.POST)
+    public ResponseEntity<String> createGame() throws HttpServerErrorException.NotImplemented {
+        return null;
+    }
+    @RequestMapping(value = "/games/{ID}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> removeGame() throws HttpServerErrorException.NotImplemented {
+        return null;
     }
 }
